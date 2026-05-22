36fb2972f5921acea51288a6dcb73f229b1916e1
leofullstack7 <lgarcessa@gmail.com>
2026-05-22 16:04:20 -0500

    feat: puertos

--- main.py
+++ main.py
@@ -198,4 +198,4 @@ async def health():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
